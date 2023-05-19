f321adfd0caeaa1ea386c9ed945e4b8740456d4a
DhruvGour <dhruv250800@gmail.com>
2023-05-20 01:37:45 +0530

    [DhruvGour]-adding addition part

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter First Number");
+int firstNumber = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Enter Second Number");
+int secondNumber = Convert.ToInt16(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+Console.WriteLine("Addition Of {0} & {1} is {2}", firstNumber, secondNumber, result);
